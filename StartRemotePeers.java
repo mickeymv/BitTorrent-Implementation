9b71c43c17fe49e1e0cd3491a56f3cdda05990c5
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2017-04-11 15:38:45 -0400

    Fix the remotePeer starter script.

--- StartRemotePeers.java
+++ StartRemotePeers.java
@@ -1,40 +1,140 @@
 
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Properties;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 
 /**
  *
- *	Run the Java program, StartRemotePeers, at the working directory as follows: > java StartRemotePeers
-
- *	The program reads file PeerInfo.cfg and starts peers specified in the file one by one. 
- *		> java peerProcess 1001
- *	It terminates after starting all peers.
+ * @author Mickey Vellukunnel
+ * 
+ *         Starts remote peers specified one by one. 
+ *         example > java PeerProcess 1001
  *
  */
 
 public class StartRemotePeers {
-		
-		private static final String peerProcessName = "java PeerProcess";
-		
-		public static void main() {
-			String workingDir = System.getProperty("user.dir");
-			
-			String peerProcessArguments = "1000"; // should be string of the peerProcessID //peer.getPeerID()
-			String hostname = "xiaolong@thunder.cise.ufl.edu"; //should be given by util method //lin114-00.cise.ufl.edu
-			//peer.getHostName()
-			
-			ArrayList<PeerInfo> peerList = Util.initializeUtil().getPeerList();
-			
-			try {
-				for(PeerInfo peer:peerList) {
-					Runtime.getRuntime().exec("ssh " + peer.getHostName() + " cd " + workingDir + " ; " +
-							peerProcessName + " " + peer.getPeerID() );
-				}
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+	// private static final String peerProcessCommandName = "java PeerProcess";
+	// private static final String projectFolderNameOnRemote = "compNetworks";
+	private static final String scriptPrefix = "cd compNetworks; java PeerProcess ";
+
+	public static class PeerInfo {
+
+		private String peerID;
+		private String hostName;
+
+		public PeerInfo(String peerID, String hostName) {
+			super();
+			this.peerID = peerID;
+			this.hostName = hostName;
 		}
+
+		public String getPeerID() {
+			return peerID;
+		}
+
+		public void setPeerID(String peerID) {
+			this.peerID = peerID;
+		}
+
+		public String getHostName() {
+			return hostName;
+		}
+
+		public void setHostName(String hostName) {
+			this.hostName = hostName;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		// String workingDir = projectFolderNameOnRemote;
+
+		ArrayList<PeerInfo> peerList = new ArrayList<>();
+
+		String ciseUser = "mvelluku"; // change with your CISE username
+
+		/**
+		 * Make sure the below peer hostnames and peerIDs match those in
+		 * PeerInfo.cfg in the remote CISE machines. Also make sure that the
+		 * peers which have the file initially have it under the 'peer_[peerID]'
+		 * folder.
+		 */
+
+		peerList.add(new PeerInfo("1", "lin114-06.cise.ufl.edu"));
+		peerList.add(new PeerInfo("2", "lin114-08.cise.ufl.edu"));
+		peerList.add(new PeerInfo("3", "lin114-09.cise.ufl.edu"));
+
+		for (PeerInfo remotePeer : peerList) {
+			try {
+				JSch jsch = new JSch();
+				jsch.addIdentity("/Users/mickey/.ssh/id_rsa", "");
+				Session session = jsch.getSession(ciseUser, remotePeer.getHostName(), 22);
+				Properties config = new Properties();
+				config.put("StrictHostKeyChecking", "no");
+				session.setConfig(config);
+
+				session.connect();
+
+				System.out.println("Session to peer# " + remotePeer.getPeerID() + " at " + remotePeer.getHostName());
+
+				Channel channel = session.openChannel("exec");
+				((ChannelExec) channel).setCommand(scriptPrefix + remotePeer.getPeerID());
+
+				channel.setInputStream(null);
+				((ChannelExec) channel).setErrStream(System.err);
+
+				InputStream input = channel.getInputStream();
+				channel.connect();
+
+				System.out.println("Channel Connected to peer# " + remotePeer.getPeerID() + " at "
+						+ remotePeer.getHostName() + " server with commands");
+
+				(new Thread() {
+					@Override
+					public void run() {
+
+						InputStreamReader inputReader = new InputStreamReader(input);
+						BufferedReader bufferedReader = new BufferedReader(inputReader);
+						String line = null;
+
+						try {
+
+							while ((line = bufferedReader.readLine()) != null) {
+								System.out.println(remotePeer.getPeerID() + ">:" + line);
+							}
+							bufferedReader.close();
+							inputReader.close();
+						} catch (Exception ex) {
+							System.out.println(remotePeer.getPeerID() + " Exception >:");
+							ex.printStackTrace();
+						}
+
+						channel.disconnect();
+						session.disconnect();
+					}
+				}).start();
+
+			} catch (JSchException e) {
+				// TODO Auto-generated catch block
+				System.out.println(remotePeer.getPeerID() + " JSchException >:");
+				e.printStackTrace();
+			} catch (IOException ex) {
+				System.out.println(remotePeer.getPeerID() + " Exception >:");
+				ex.printStackTrace();
+			}
+
+		}
+	}
+
 }
